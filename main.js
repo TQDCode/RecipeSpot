5f094a17749bab16faae7d2b55cdb4a033802b86
Worthy <worthyintech@gmail.com>
2024-04-11 14:24:02 +0100

    Updating progress.

--- main.js
+++ main.js
@@ -9,6 +9,12 @@ function handleEnterKeyPress(event) {
   }
 }
 
+function searchRecipes(search) {
+  const apiURL = `www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
+
+  console.log(apiURL);
+}
+
 // The key press is only valid if there has been a search.
 searchBar.addEventListener("change", () => {
   // Removing the event listener for the Enter key press if it was previously added.
@@ -18,4 +24,6 @@ searchBar.addEventListener("change", () => {
   document.addEventListener("keypress", handleEnterKeyPress);
 
   console.log("Search bar value changed.");
+
+  searchRecipes(searchBar.value);
 });
