b79f0912d3720eefd2b6a48a7daf0eb3c676534c
Worthy <worthyintech@gmail.com>
2024-04-11 14:14:35 +0100

    Updating progress.

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("Testing...");
+const searchBar = document.querySelector("#searchbar");
+console.log(searchBar);
+
+// Functions that handles the 'Enter' key press.
+function handleEnterKeyPress(event) {
+  // Checking whether the key pressed is the enter key.
+  if (event.key === "Enter") {
+    console.log("Enter key clicked.");
+  }
+}
+
+// The key press is only valid if there has been a search.
+searchBar.addEventListener("change", () => {
+  // Removing the event listener for the Enter key press if it was previously added.
+  document.removeEventListener("keypress", handleEnterKeyPress);
+
+  // Adding the event listener if the search bar value changes.
+  document.addEventListener("keypress", handleEnterKeyPress);
+
+  console.log("Search bar value changed.");
+});
