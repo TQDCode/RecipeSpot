4ff5609c07126ee2a74054c411d8bbd5b1e01d02
Worthy <worthyintech@gmail.com>
2024-04-11 18:37:09 +0100

    Updating progress.

--- scripts/buildRecipe.js
+++ scripts/buildRecipe.js
@@ -1,3 +1,11 @@
 const recipe = JSON.parse(sessionStorage.getItem("selectedRecipe"));
 
 console.log(recipe);
+
+const recipeSection = document.querySelector(".individual-recipe");
+
+// Updating the image.
+const img = recipeSection.querySelector("img");
+img.setAttribute("src", recipe.strMealThumb);
+
+console.log(img);
